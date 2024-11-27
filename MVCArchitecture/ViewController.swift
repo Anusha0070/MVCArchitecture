db186a50e488cd138b75ebebef380620f3506758
Anusha Raju <anusha.raju0070@gmail.com>
2024-11-27 10:40:48 -0800

    Initial Commit

---
+++ MVCArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVCArchitecture
+//
+//  Created by Anusha Raju on 11/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MVCArchitectureTests/MVCArchitectureTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVCArchitectureTests.swift
+//  MVCArchitectureTests
+//
+//  Created by Anusha Raju on 11/27/24.
+//
+
+import Testing
+@testable import MVCArchitecture
+
+struct MVCArchitectureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
